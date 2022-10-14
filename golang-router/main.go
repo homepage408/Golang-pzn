d6c050eb1290e198dae4c8e49b96a79fce542043
homepage408 <homepage408@gmail.com>
2022-10-14 21:03:32 +0700

    golang router

---
+++ golang-router/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func main() {
+	router := httprouter.New()
+
+	server := http.Server{
+		Handler: router,
+		Addr:    "localhost:8080",
+	}
+
+	server.ListenAndServe()
+
+}
