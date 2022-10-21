89dd7ee019ef22b0bf0e1adaa8354008b7ca1f8f
homepage408 <homepage408@gmail.com>
2022-10-21 18:25:34 +0700

    add golang rest api

---
+++ golang-restfull-api/model/domain/category.go
@@ -0,0 +1,9 @@
+package domain
+
+type Category struct {
+	Id        int    `json:"id"`
+	Name      string `json:"name"`
+	CreatedAt string `json:"created_at"`
+	UpdatedAt string `json:"updated_at"`
+	DeletedAt string `json:"deleted_at"`
+}
---
+++ golang-restfull-api/service/category_service_impl.go
@@ -0,0 +1,87 @@
+package service
+
+import (
+	"context"
+	"database/sql"
+	"golang-restfull-api/helper"
+	"golang-restfull-api/model/domain"
+	"golang-restfull-api/model/web"
+	"golang-restfull-api/repository"
+
+	"github.com/go-playground/validator/v10"
+)
+
+type CategoryServiceImpl struct {
+	CategoryRepository repository.CategoryRepository
+	DB                 *sql.DB
+	Validate           *validator.Validate
+}
+
+func NewCategoryService(categoryRespository repository.CategoryRepository, DB *sql.DB, validate *validator.Validate) CategoryService {
+	return &CategoryServiceImpl{
+		CategoryRepository: categoryRespository,
+		DB:                 DB,
+		Validate:           validate,
+	}
+}
+
+func (service *CategoryServiceImpl) Create(ctx context.Context, req web.CategoryCreateRequest) web.CategoryResponse {
+	err := service.Validate.Struct(req)
+	helper.PanicIfError(err)
+
+	tx, err := service.DB.Begin()
+	helper.PanicIfError(err)
+	defer helper.CommitOrRollback(tx)
+
+	category := domain.Category{
+		Name: req.Name,
+	}
+
+	category = service.CategoryRepository.Save(ctx, tx, category)
+	return helper.ToCategoryResponse(category)
+}
+
+func (service *CategoryServiceImpl) Update(ctx context.Context, req web.CategoryUpdateRequest) web.CategoryResponse {
+	err := service.Validate.Struct(req)
+	helper.PanicIfError(err)
+
+	tx, err := service.DB.Begin()
+	helper.PanicIfError(err)
+	defer helper.CommitOrRollback(tx)
+
+	category, err := service.CategoryRepository.FindById(ctx, tx, req.Id)
+	helper.PanicIfError(err)
+
+	category.Name = req.Name
+	category = service.CategoryRepository.Update(ctx, tx, category)
+	return helper.ToCategoryResponse(category)
+}
+func (service *CategoryServiceImpl) Delete(ctx context.Context, categoryId int) {
+	tx, err := service.DB.Begin()
+	helper.PanicIfError(err)
+	defer helper.CommitOrRollback(tx)
+
+	category, err := service.CategoryRepository.FindById(ctx, tx, categoryId)
+	helper.PanicIfError(err)
+
+	service.CategoryRepository.Update(ctx, tx, category)
+}
+func (service *CategoryServiceImpl) FindById(ctx context.Context, categoryId int) web.CategoryResponse {
+	tx, err := service.DB.Begin()
+	helper.PanicIfError(err)
+	defer helper.CommitOrRollback(tx)
+
+	category, err := service.CategoryRepository.FindById(ctx, tx, categoryId)
+	helper.PanicIfError(err)
+
+	return helper.ToCategoryResponse(category)
+}
+func (service *CategoryServiceImpl) FindAll(ctx context.Context) []web.CategoryResponse {
+	tx, err := service.DB.Begin()
+	helper.PanicIfError(err)
+	defer helper.CommitOrRollback(tx)
+
+	categories := service.CategoryRepository.FindAll(ctx, tx)
+
+	return helper.ToCategoryResponses(categories)
+}
